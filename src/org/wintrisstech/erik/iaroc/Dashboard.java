800ad30513dc7defa31f37c74783a3ae9e247225
Erik Colban <erik.colban@drawmetry.com>
2013-06-14 20:09:58 -0700

    Added compass and new logo

--- src/org/wintrisstech/erik/iaroc/Dashboard.java
+++ src/org/wintrisstech/erik/iaroc/Dashboard.java
@@ -2,6 +2,10 @@ package org.wintrisstech.erik.iaroc;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.widget.ScrollView;
@@ -11,167 +15,254 @@ import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
 import java.util.Locale;
+import org.wintrisstech.vw.SimpleVicswagen;
 import org.wintrisstech.vw.VicswagenInterface;
 
 /**
  * This is the main activity of the iRobot2012 application.
- *
- * <p>This class assumes that there are 3 ultrasonic sensors attached to the
- * iRobot. An instance of the Dashboard class will display the readings of these
- * three sensors.
- *
- * <p> There should be no need to modify this class. Modify VW_Prototype instead.
- *
+ * 
+ * <p>
+ * This class assumes that there are 4 ultrasonic sensors attached to the robot.
+ * An instance of the Dashboard class will display the readings of these 4
+ * sensors.
+ * 
+ * <p>
+ * There should be no need to modify this class. Modify VW_Prototype instead.
+ * 
  * @author Erik Colban
- *
+ * 
  */
-public class Dashboard extends IOIOActivity implements TextToSpeech.OnInitListener
-{
+public class Dashboard extends IOIOActivity implements
+		TextToSpeech.OnInitListener, SensorEventListener {
 
-    /**
-     * Tag used for debugging.
-     */
-    private static final String TAG = "Dashboard";
-    /**
-     * Text view that contains all logged messages
-     */
-    private TextView mText;
-    private ScrollView scroller;
-    /**
-     * A VW_Prototype instance
-     */
-    private VW_Prototype stuntman;
-    /**
-     * TTS stuff
-     */
-    protected static final int MY_DATA_CHECK_CODE = 33;
-    private TextToSpeech mTts;
+	/**
+	 * Tag used for debugging.
+	 */
+	private static final String TAG = "Dashboard";
+	/**
+	 * Text view that contains all logged messages
+	 */
+	private TextView mText;
+	private ScrollView scroller;
+	/**
+	 * A VW_Prototype instance
+	 */
+	private VW_Prototype stuntman;
+	/**
+	 * TTS stuff
+	 */
+	protected static final int MY_DATA_CHECK_CODE = 33;
+	private TextToSpeech mTts;
+	/**
+	 * Compass stuff
+	 */
+	SensorManager sensorManager;
+	private Sensor sensorAccelerometer;
+	private Sensor sensorMagneticField;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        /*
-         * Since the android device is carried by the iRobot Create, we want to
-         * prevent a change of orientation, which would cause the activity to
-         * pause.
-         */
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.main);
+	private float[] valuesAccelerometer;
+	private float[] valuesMagneticField;
 
-        Intent checkIntent = new Intent();
-        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-        startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
+	private float[] matrixR;
+	private float[] matrixI;
+	private float[] matrixValues;
+	private double azimuth;
+	private double pitch;
+	private double roll;
 
-        mText = (TextView) findViewById(R.id.text);
-        scroller = (ScrollView) findViewById(R.id.scroller);
-        log(getString(R.string.wait_ioio));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*
+		 * Since the android device is carried by the iRobot Create, we want to
+		 * prevent a change of orientation, which would cause the activity to
+		 * pause.
+		 */
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		// getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setContentView(R.layout.main);
 
-    }
+		Intent checkIntent = new Intent();
+		checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+		startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
 
-    @Override
-    public void onPause()
-    {
-        if (stuntman != null)
-        {
-            log("Pausing");
-        }
-        super.onPause();
-    }
+		// Compass stuff
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		sensorAccelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorMagneticField = sensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == MY_DATA_CHECK_CODE)
-        {
-            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS)
-            {
-                // success, create the TTS instance
-                mTts = new TextToSpeech(this, this);
-            } else
-            {
-                // missing data, install it
-                Intent installIntent = new Intent();
-                installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-                startActivity(installIntent);
-            }
-        }
-    }
+		valuesAccelerometer = new float[3];
+		valuesMagneticField = new float[3];
 
-    public void onInit(int arg0)
-    {
-    }
+		matrixR = new float[9];
+		matrixI = new float[9];
+		matrixValues = new float[3];
 
-    public void speak(String stuffToSay)
-    {
-        mTts.setLanguage(Locale.US);
-        if (!mTts.isSpeaking())
-        {
-            mTts.speak(stuffToSay, TextToSpeech.QUEUE_FLUSH, null);
-        }
-    }
+		mText = (TextView) findViewById(R.id.text);
+		scroller = (ScrollView) findViewById(R.id.scroller);
+		log(getString(R.string.wait_ioio));
 
-    @Override
-    public IOIOLooper createIOIOLooper()
-    {
-        return new IOIOLooper()
-        {
+	}
 
-            public void setup(IOIO ioio) throws ConnectionLostException, InterruptedException
-            {
-                /*
-                 * When the setup() method is called the IOIO is connected.
-                 */
-                log(getString(R.string.ioio_connected));
+	@Override
+	public void onPause() {
+		if (stuntman != null) {
+			log("Pausing");
+		}
+		sensorManager.unregisterListener(this, sensorAccelerometer);
+		sensorManager.unregisterListener(this, sensorMagneticField);
+		super.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
 
-                /*
-                 * Establish communication between the android and the iRobot
-                 * Create through the IOIO board.
-                 */
-                log(getString(R.string.wait_create));
-                VicswagenInterface vicswagen = new SimpleVicswagenDebug(ioio, Dashboard.this);
-                log(getString(R.string.create_connected));
+		sensorManager.registerListener(this, sensorAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, sensorMagneticField,
+				SensorManager.SENSOR_DELAY_NORMAL);
+		super.onResume();
+	}
 
-                /*
-                 * Get a VW_Prototype and let it go... 
-                 */
-                stuntman = new VW_Prototype(vicswagen, Dashboard.this);
-                stuntman.initialize();
-            }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == MY_DATA_CHECK_CODE) {
+			if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+				// success, create the TTS instance
+				mTts = new TextToSpeech(this, this);
+			} else {
+				// missing data, install it
+				Intent installIntent = new Intent();
+				installIntent
+						.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+				startActivity(installIntent);
+			}
+		}
+	}
 
-            public void loop() throws ConnectionLostException, InterruptedException
-            {
-                stuntman.loop();
-            }
+	public void onInit(int arg0) {
+	}
 
-            public void disconnected()
-            {
-                log(getString(R.string.ioio_disconnected));
-            }
+	public void speak(String stuffToSay) {
+		mTts.setLanguage(Locale.US);
+		if (!mTts.isSpeaking()) {
+			mTts.speak(stuffToSay, TextToSpeech.QUEUE_FLUSH, null);
+		}
+	}
 
-            public void incompatible()
-            {
-            }
-        };
-    }
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
 
-    /**
-     * Writes a message to the Dashboard instance.
-     *
-     * @param msg the message to write
-     */
-    public void log(final String msg)
-    {
-        runOnUiThread(new Runnable()
-        {
+	}
 
-            public void run()
-            {
-                mText.append(msg);
-                mText.append("\n");
-                scroller.smoothScrollTo(0, mText.getBottom()); 
-            }
-        });
-    }
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		switch (event.sensor.getType()) {
+		case Sensor.TYPE_ACCELEROMETER:
+			for (int i = 0; i < 3; i++) {
+				valuesAccelerometer[i] = event.values[i];
+			}
+			break;
+		case Sensor.TYPE_MAGNETIC_FIELD:
+			for (int i = 0; i < 3; i++) {
+				valuesMagneticField[i] = event.values[i];
+			}
+			break;
+		}
+
+		boolean success = SensorManager.getRotationMatrix(matrixR, matrixI,
+				valuesAccelerometer, valuesMagneticField);
+
+		if (success) {
+			SensorManager.getOrientation(matrixR, matrixValues);
+			synchronized (this) {
+				azimuth = Math.toDegrees(matrixValues[0]);
+				pitch = Math.toDegrees(matrixValues[1]);
+				roll = Math.toDegrees(matrixValues[2]);
+			}
+		}
+
+	}
+
+	/**
+	 * Gets the azimuth
+	 * @return the azimuth
+	 */
+	public synchronized double getAzimuth() {
+		return azimuth;
+	}
+	
+	/**
+	 * Gets the pitch
+	 * @return the pitch
+	 */
+	public synchronized double getPitch() {
+		return pitch;
+	}
+
+	/**
+	 */
+	public synchronized double getRoll() {
+		return roll;
+	}
+	
+	@Override
+	public IOIOLooper createIOIOLooper() {
+		return new IOIOLooper() {
+
+			public void setup(IOIO ioio) throws ConnectionLostException,
+					InterruptedException {
+				/*
+				 * When the setup() method is called the IOIO is connected.
+				 */
+				log(getString(R.string.ioio_connected));
+
+				/*
+				 * Establish communication between the android and the iRobot
+				 * Create through the IOIO board.
+				 */
+				log(getString(R.string.wait_create));
+				VicswagenInterface vicswagen = new SimpleVicswagen(ioio);
+				log(getString(R.string.create_connected));
+
+				/*
+				 * Get a VW_Prototype and let it go...
+				 */
+				stuntman = new VW_Prototype(vicswagen, Dashboard.this);
+				stuntman.initialize();
+			}
+
+			public void loop() throws ConnectionLostException,
+					InterruptedException {
+				stuntman.loop();
+			}
+
+			public void disconnected() {
+				log(getString(R.string.ioio_disconnected));
+			}
+
+			public void incompatible() {
+			}
+		};
+	}
+
+	/**
+	 * Writes a message to the Dashboard instance.
+	 * 
+	 * @param msg
+	 *            the message to write
+	 */
+	public void log(final String msg) {
+		runOnUiThread(new Runnable() {
+
+			public void run() {
+				mText.append(msg);
+				mText.append("\n");
+				scroller.smoothScrollTo(0, mText.getBottom());
+			}
+		});
+	}
 }
--- src/org/wintrisstech/erik/iaroc/SimpleVicswagenDebug.java
+++ src/org/wintrisstech/erik/iaroc/SimpleVicswagenDebug.java
@@ -13,220 +13,222 @@ import ioio.lib.api.exception.ConnectionLostException;
 
 /**
  * A final class that implements the VicswagenInterface.
- *
+ * 
  */
-public final class SimpleVicswagenDebug implements VicswagenInterface
-{
+public final class SimpleVicswagenDebug implements VicswagenInterface {
 
+	private static final float PULSE_LENGTH_TO_DISTANCE = 18000F;
+	/* Instance variables */
+	private final IOIO ioio;
+	private final Dashboard dashboard;
+	private int frontDistance;
+	private PwmOutput leftPwmOutput;
+	private PwmOutput rightPwmOutput;
+	private boolean leftPwmOutputOpen = false;
+	private boolean rightPwmOutputOpen = false;
+	private PulseInput frontDistanceInput;
+	private PulseInput rearDistanceInput;
+	private PulseInput leftDistanceInput;
+	private PulseInput rightDistanceInput;
+	private DigitalOutput frontStrobe;
+	private DigitalOutput rearStrobe;
+	private DigitalOutput leftStrobe;
+	private DigitalOutput rightStrobe;
+	/* The following variables require calibration */
+    private float angularVelocityFactor = 0.00312F; //
+    private float velocityFactor = 0.545F;
+	/**
+	 * Must be true for motors to run. high = slow decay, low = fast decay
+	 */
+	private DigitalOutput motorEnable;
+	private DigitalOutput rightDirection;
+	private DigitalOutput leftDirection;
+	private AnalogInput inX;
+	private AnalogInput inY;
+	private AnalogInput inZ;
+	private long startTime;
+	private float leftVelocity;
+	private float rightVelocity;
 
-    /* The following static variable require calibration */
-    private static final float WHEEL_RADIUS = 60; // in mm
-    private static final float WHEEL_DISTANCE = 400; //in mm
-    private static final int VELOCITY_TO_FREQUENCY =
-            (int) (400F / Math.PI * WHEEL_RADIUS);
-    private float PULSE_LENGTH_TO_DISTANCE = 18000F;
-    /* Instance variables */
-    private final IOIO ioio;
-    private final Dashboard dashboard;
-    private int frontDistance;
-    private PwmOutput leftPwmOutput;
-    private PwmOutput rightPwmOutput;
-    private boolean leftPwmOutputOpen = false;
-    private boolean rightPwmOutputOpen = false;
-    private PulseInput frontDistanceInput;
-    private PulseInput rearDistanceInput;
-    private PulseInput leftDistanceInput;
-    private PulseInput rightDistanceInput;
-    private DigitalOutput frontStrobe;
-    private DigitalOutput rearStrobe;
-    private DigitalOutput leftStrobe;
-    private DigitalOutput rightStrobe;
-    private DigitalOutput rightDirection;
-    private DigitalOutput leftDirection;
-    private AnalogInput inX;
-    private AnalogInput inY;
-    private AnalogInput inZ;
-    private long startTime;
-    private float leftVelocity;
-    private float rightVelocity;
+	public SimpleVicswagenDebug(IOIO ioio, Dashboard dashboard)
+			throws ConnectionLostException {
+		this.ioio = ioio;
+		this.dashboard = dashboard;
+		this.initialize();
+	}
 
-    public SimpleVicswagenDebug(IOIO ioio, Dashboard dashboard) throws ConnectionLostException
-    {
-        this.ioio = ioio;
-        this.dashboard = dashboard;
-        this.initialize();
+	private void initialize() throws ConnectionLostException {
+		dashboard.log("In initialize()");
+		rearStrobe = ioio.openDigitalOutput(REAR_STROBE_ULTRASONIC_OUTPUT_PIN);
+		rearStrobe.write(false);
+		frontStrobe = ioio
+				.openDigitalOutput(FRONT_STROBE_ULTRASONIC_OUTPUT_PIN);
+		frontStrobe.write(false);
+		leftStrobe = ioio.openDigitalOutput(LEFT_STROBE_ULTRASONIC_OUTPUT_PIN);
+		leftStrobe.write(false);
+		rightStrobe = ioio
+				.openDigitalOutput(RIGHT_STROBE_ULTRASONIC_OUTPUT_PIN);
+		rightStrobe.write(false);
+
+		frontDistanceInput = ioio.openPulseInput(FRONT_ULTRASONIC_INPUT_PIN,
+				PulseInput.PulseMode.POSITIVE);
+		leftDistanceInput = ioio.openPulseInput(LEFT_ULTRASONIC_INPUT_PIN,
+				PulseInput.PulseMode.POSITIVE);
+		rightDistanceInput = ioio.openPulseInput(RIGHT_ULTRASONIC_INPUT_PIN,
+				PulseInput.PulseMode.POSITIVE);
+		// Single precision for the rear sensor because there is maximum 3
+		// double precision modules.
+		rearDistanceInput = ioio.openPulseInput(new DigitalInput.Spec(
+				REAR_ULTRASONIC_INPUT_PIN), PulseInput.ClockRate.RATE_2MHz,
+				PulseInput.PulseMode.POSITIVE, false);
+
+		inX = ioio.openAnalogInput(X_AXIS_ANALOG_INPUT_PIN);
+		inY = ioio.openAnalogInput(Y_AXIS_ANALOG_INPUT_PIN);
+		inZ = ioio.openAnalogInput(Z_AXIS_ANALOG_INPUT_PIN);
+		dashboard.log("Enabling motors");
+		motorEnable = ioio.openDigitalOutput(MOTOR_ENABLE_PIN);
+		motorEnable.write(true);
+		rightDirection = ioio.openDigitalOutput(RIGHT_DIRECTION_OUTPUT_PIN);
+		leftDirection = ioio.openDigitalOutput(LEFT_DIRECTION_OUTPUT_PIN);
+	}
+	
+	public void calibrateVelocity(float factor) {
+    	velocityFactor = factor;
+    }
+    
+    public void calibrateAngularVelocity(float factor) {
+    	angularVelocityFactor = factor;
     }
 
-    private void initialize() throws ConnectionLostException
-    {
-    	dashboard.log("In initialize()");
-        rearStrobe = ioio.openDigitalOutput(REAR_STROBE_ULTRASONIC_OUTPUT_PIN);
-        rearStrobe.write(false);
-        frontStrobe = ioio.openDigitalOutput(FRONT_STROBE_ULTRASONIC_OUTPUT_PIN);
-        frontStrobe.write(false);
-        leftStrobe = ioio.openDigitalOutput(LEFT_STROBE_ULTRASONIC_OUTPUT_PIN);
-        leftStrobe.write(false);
-        rightStrobe = ioio.openDigitalOutput(RIGHT_STROBE_ULTRASONIC_OUTPUT_PIN);
-        rightStrobe.write(false);
+	public void driveDirect(int leftVelocity, int rightVelocity)
+			throws ConnectionLostException {
+		dashboard.log("VELOCITY_TO_FREQUENCY = " + velocityFactor);
+		dashboard.log("leftVelocity = " + leftVelocity);
+		dashboard.log("rightVelocity = " + rightVelocity);
+		leftDirection.write(leftVelocity < 0);
+		rightDirection.write(rightVelocity < 0);
+		int leftFrequency = (int) (Math.abs(leftVelocity) * velocityFactor);
+		int rightFrequency = (int) (Math.abs(rightVelocity) * velocityFactor);
+		dashboard.log("leftFrequency = " + leftFrequency);
+		dashboard.log("rightFrequency = " + rightFrequency);
+		if (leftPwmOutputOpen) {
+			leftPwmOutput.close();
+			leftPwmOutputOpen = false;
+		}
+		if (rightPwmOutputOpen) {
+			rightPwmOutput.close();
+			rightPwmOutputOpen = false;
+		}
 
-        frontDistanceInput = ioio.openPulseInput(FRONT_ULTRASONIC_INPUT_PIN, PulseInput.PulseMode.POSITIVE);
-        leftDistanceInput = ioio.openPulseInput(LEFT_ULTRASONIC_INPUT_PIN, PulseInput.PulseMode.POSITIVE);
-        rightDistanceInput = ioio.openPulseInput(RIGHT_ULTRASONIC_INPUT_PIN, PulseInput.PulseMode.POSITIVE);
-        //Single precision for the rear sensor beacuse there is maximum 3 double precision modules.
-        rearDistanceInput = ioio.openPulseInput(
-                new DigitalInput.Spec(REAR_ULTRASONIC_INPUT_PIN),
-                PulseInput.ClockRate.RATE_2MHz,
-                PulseInput.PulseMode.POSITIVE, false);
+		leftPwmOutput = ioio.openPwmOutput(LEFT_PWM_OUTPUT_PIN, leftFrequency);
+		leftPwmOutputOpen = true;
+		rightPwmOutput = ioio.openPwmOutput(RIGHT_PWM_OUTPUT_PIN,
+				rightFrequency);
+		rightPwmOutputOpen = true;
+		leftPwmOutput.setDutyCycle(0.1F);
+		rightPwmOutput.setDutyCycle(0.1F);
+		startTime = SystemClock.elapsedRealtime();
+		this.leftVelocity = leftVelocity;
+		this.rightVelocity = rightVelocity;
 
-        inX = ioio.openAnalogInput(X_AXIS_ANALOG_INPUT_PIN);
-        inY = ioio.openAnalogInput(Y_AXIS_ANALOG_INPUT_PIN);
-        inZ = ioio.openAnalogInput(Z_AXIS_ANALOG_INPUT_PIN);
+	}
 
-        this.rightDirection = ioio.openDigitalOutput(RIGHT_DIRECTION_OUTPUT_PIN);
-        this.leftDirection = ioio.openDigitalOutput(LEFT_DIRECTION_OUTPUT_PIN);
-    }
+	/**
+	 * Get the angle since the last driveDirect call. The angle is negative if
+	 * turning clockwise, positive if turning counter-clockwise, 0 if moving
+	 * straight.
+	 * 
+	 * @return the angle in degrees
+	 */
+	public int getAngle() {
+		if (leftVelocity == rightVelocity) {
+			return 0;
+		}
+		float angularVelocity = (rightVelocity - leftVelocity) * angularVelocityFactor;
+		float angleRadians = (SystemClock.elapsedRealtime() - startTime)
+				* angularVelocity / 1000;
+		return (int) (Math.toDegrees(angleRadians));
+	}
 
-    public void driveDirect(int leftVelocity, int rightVelocity) throws ConnectionLostException
-    {
-        leftDirection.write(leftVelocity > 0);
-        rightDirection.write(rightVelocity > 0);
-        int leftFrequency = Math.abs(leftVelocity) * VELOCITY_TO_FREQUENCY;
-        int rightFrequency = Math.abs(rightVelocity) * VELOCITY_TO_FREQUENCY;
-        if (leftPwmOutputOpen)
-        {
-            leftPwmOutput.close();
-            leftPwmOutputOpen = false;
-        }
-        if (rightPwmOutputOpen)
-        {
-            rightPwmOutput.close();
-            rightPwmOutputOpen = false;
-        }
+	public int getLeftWheelDistance() {
+		return (int) ((SystemClock.elapsedRealtime() - startTime)
+				* leftVelocity / 1000);
+	}
 
-        leftPwmOutput = ioio.openPwmOutput(LEFT_PWM_OUTPUT_PIN, leftFrequency);
-        rightPwmOutput = ioio.openPwmOutput(RIGHT_PWM_OUTPUT_PIN, rightFrequency);
-        leftPwmOutput.setDutyCycle(0.1F);
-        rightPwmOutput.setDutyCycle(0.1F);
-        startTime = SystemClock.elapsedRealtime();
-        this.leftVelocity = leftVelocity;
-        this.rightVelocity = rightVelocity;
+	public int getRightWheelDistance() {
+		return (int) ((SystemClock.elapsedRealtime() - startTime)
+				* rightVelocity / 1000);
+	}
 
-    }
+	public int getInfraredByte() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    /**
-     * Get the angle since the last driveDirect call. The angle is negative if
-     * turning clockwise, positive if turning counter-clockwise, 0 if moving
-     * straight.
-     *
-     * @return the angle in degrees
-     */
-    public int getAngle()
-    {
-        if (leftVelocity == rightVelocity)
-        {
-            return 0;
-        }
-        float angularVelocity = (rightVelocity - leftVelocity) / WHEEL_DISTANCE;
-        float angleRadians = (SystemClock.elapsedRealtime() - startTime) * angularVelocity;
-        return (int) (Math.toDegrees(angleRadians));
-    }
+	public int getLeftVelocity() {
+		return (int) leftVelocity;
+	}
 
-    public int getLeftWheelDistance()
-    {
-        return (int) ((SystemClock.elapsedRealtime() - startTime) * leftVelocity);
-    }
+	public int getRightVelocity() {
+		return (int) rightVelocity;
+	}
 
-    public int getRightWheelDistance()
-    {
-        return (int) ((SystemClock.elapsedRealtime() - startTime) * rightVelocity);
-    }
+	/**
+	 * Gets the turn radius. If the robot is turning on the spot, the turn
+	 * radius is 0. If the robot is going straight, then Integer.MAX_VALUE is
+	 * returned. Otherwise, the radius is positive if turning counter-clockwise,
+	 * negative is turning clockwise.
+	 * 
+	 * @return the turn radius in mm
+	 */
+	public int getTurnRadius() {
+		if (leftVelocity == rightVelocity) {
+			return Integer.MAX_VALUE;
+		}
 
-    public int getInfraredByte()
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+		return (int) ((leftVelocity + rightVelocity)
+				/ (rightVelocity - leftVelocity) / angularVelocityFactor / 2);
+	}
 
-    public int getLeftVelocity()
-    {
-        return (int) leftVelocity;
-    }
+	public int getFrontUltrasonicDistance() throws InterruptedException {
+		try {
+			frontStrobe.write(true);
+			frontStrobe.write(false);
+			frontDistance = (int) (frontDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
+		} catch (ConnectionLostException ex) {
+		}
+		return frontDistance;
+	}
 
-    public int getRightVelocity()
-    {
-        return (int) rightVelocity;
-    }
+	public int getRearUltrasonicDistance() throws InterruptedException {
+		int rearDistance = 0;
+		try {
+			rearStrobe.write(true);
+			rearStrobe.write(false);
+			rearDistance = (int) (rearDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
+		} catch (ConnectionLostException ex) {
+		}
+		return rearDistance;
+	}
 
-    /**
-     * Gets the turn radius. If the robot is turning on the spot, the turn
-     * radius is 0. If the robot is going straight, then Integer.MAX_VALUE is
-     * returned. Otherwise, the radius is positive if turning counter-clockwise,
-     * negative is turning clockwise.
-     *
-     * @return the turn radius in mm
-     */
-    public int getTurnRadius()
-    {
-        if (leftVelocity == rightVelocity)
-        {
-            return Integer.MAX_VALUE;
-        }
+	public int getLeftUltrasonicDistance() throws InterruptedException {
+		int leftDistance = 0;
+		try {
+			leftStrobe.write(true);
+			leftStrobe.write(false);
+			leftDistance = (int) (leftDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
+		} catch (ConnectionLostException ex) {
+		}
+		return leftDistance;
+	}
 
-        return (int) ((leftVelocity + rightVelocity)
-                / (rightVelocity - leftVelocity) * WHEEL_DISTANCE / 2);
-    }
-
-    public int getFrontUltrasonicDistance() throws InterruptedException
-    {
-        try
-        {
-            frontStrobe.write(true);
-            frontStrobe.write(false);
-            frontDistance = (int) (frontDistanceInput.waitPulseGetDuration() * 18000);
-        } catch (ConnectionLostException ex)
-        {
-        }
-        return frontDistance;
-    }
-
-    public int getRearUltrasonicDistance() throws InterruptedException
-    {
-        int rearDistance = 0;
-        try
-        {
-            rearStrobe.write(true);
-            rearStrobe.write(false);
-            rearDistance = (int) (rearDistanceInput.waitPulseGetDuration() * 18000);
-        } catch (ConnectionLostException ex)
-        {
-        }
-        return rearDistance;
-    }
-
-    public int getLeftUltrasonicDistance() throws InterruptedException
-    {
-        int leftDistance = 0;
-        try
-        {
-            leftStrobe.write(true);
-            leftStrobe.write(false);
-            leftDistance = (int) (leftDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
-        } catch (ConnectionLostException ex)
-        {
-        }
-        return leftDistance;
-    }
-
-    public int getRightUltrasonicDistance() throws InterruptedException
-    {
-        int rightDistance = 0;
-        try
-        {
-            rightStrobe.write(true);
-            rightStrobe.write(false);
-            rightDistance = (int) (rightDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
-        } catch (ConnectionLostException ex)
-        {
-        }
-        return rightDistance;
-    }
+	public int getRightUltrasonicDistance() throws InterruptedException {
+		int rightDistance = 0;
+		try {
+			rightStrobe.write(true);
+			rightStrobe.write(false);
+			rightDistance = (int) (rightDistanceInput.waitPulseGetDuration() * PULSE_LENGTH_TO_DISTANCE);
+		} catch (ConnectionLostException ex) {
+		}
+		return rightDistance;
+	}
 }
